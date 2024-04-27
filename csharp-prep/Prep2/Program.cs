fbcb7637eb3ae6199381b94ffe60bdfa4158203f
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-04-27 14:48:13 -0600

    created a grader that tells a student their over all grade

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Please enter your grade: ");
+        string grade = Console.ReadLine();
+        int number_grade = int.Parse(grade);
+        string letter = "Z";
+
+        if (number_grade >=70)
+        {
+            if (number_grade >=90)
+            {
+                letter = "A";
+            }
+            else if (number_grade >= 80)
+            {
+                letter = "B";
+            }
+            letter = "C";
+        }
+        else if (number_grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (number_grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You Passed the class!");
+            
+        }
+        else
+        {
+            Console.WriteLine("Dont worry, try the class again you are sure to pass!");
+            
+        }
+
     }
 }
