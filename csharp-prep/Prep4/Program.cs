62c66ee71261132fbc745648f09f9088e90edbd5
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-05-02 17:51:08 -0600

    added a large number

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,33 +2,40 @@ using System;
 
 class Program
 {
-    static int Average(List<int> numbers)
-    {
-        int sum = 0;
-        int count = 0;
-        int average = 0;
-        foreach(int num in numbers)
-        {
-            count += 1;
-            sum += num;
-        } 
-        average = sum/count;
-        return average;
-    }
+    // static int Average(List<int> numbers)
+    // {
+    //     int sum = 0;
+    //     int count = 0;
+
+    //     foreach(int num in numbers)
+    //     {
+    //         count += 1;
+    //         sum += num;
+    //     } 
+    //     int average = sum/count;
+    //     return average;
+    // }
     
     static void Main(string[] args)
     {
         List<int>numbers = new();
         int number; // variable for the input number of the user
         int sum = 0; //variable for the sum  
+        int largeNumber  = 0;
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
         do
         {
         Console.Write("Enter number: ");
         number = int.Parse(Console.ReadLine());
-        numbers.Add(number);
-
+        if (number > largeNumber)
+        {
+            largeNumber = number;
+        }
+        if (number != 0)
+        {
+            numbers.Add(number);
+        }
         } while (number != 0);
 
         for (int i = 0; i < numbers.Count; i++)
@@ -36,8 +43,9 @@ class Program
             sum += numbers[i]; //This will acumulate the sum
         }
         Console.WriteLine($"The sum is: {sum}");
-        int average = Average(numbers);
+        float average = sum / numbers.Count;
 
         Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largeNumber}");
     }
 }
