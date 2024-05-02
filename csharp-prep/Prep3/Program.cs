da7d86c75b845869832b99d8c6fdc5e3b99f8275
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-05-02 17:28:19 -0600

    discard repeating line

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -7,6 +7,7 @@ class Program
         Random random = new();
         int MagicNumber = random.Next(1,101);
         int NumberGuess;
+        Console.WriteLine("Guess a number between 1 and 100");
         do
         {    
             Console.Write("Guess a number between 1 and 100");
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,15 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        List<int>numbers;
-        numbers = new[];
+        List<int>numbers = new();
         int number;
 
         Console.WriteLine("Enter a list of numbers, type 0 when finished");
         do
         {
         Console.Write("Enter number");
-        number = Console.ReadLine();
+        number = int.Parse(Console.ReadLine());
         numbers.Add(number);
         } while (number != 0);
     }
