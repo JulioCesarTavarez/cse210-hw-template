d2f0380301f91bb9119c2a34476e6ec5b3422adf
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-05-18 08:12:12 -0600

    ending the loop

--- prove/Develop02/menu.cs
+++ prove/Develop02/menu.cs
@@ -7,45 +7,47 @@ public class Mainmenu
     static void Main()
     {
         bool loop = true;
-        while (loop == true){
-        // display options
-        Journal journal = new();
-        Console.WriteLine("\nWelcome to the Journal Program!");
-        Console.WriteLine("Please select one of the following choices");
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quite");
-        Console.Write("What would you like to do? ");
-        int choice = int.Parse(Console.ReadLine());
+        while (loop == true) // this wil continue to run till the user enters choise 5 making it false
+        {
+                // display options
+                Journal journal = new();
+                Console.WriteLine("\nWelcome to the Journal Program!");
+                Console.WriteLine("Please select one of the following choices");
+                Console.WriteLine("1. Write");
+                Console.WriteLine("2. Display");
+                Console.WriteLine("3. Load");
+                Console.WriteLine("4. Save");
+                Console.WriteLine("5. Quite");
+                Console.Write("What would you like to do? ");
+                int choice = int.Parse(Console.ReadLine());
 
-    if (choice == 1) // if they enter one they write an entry.
-        {
-            journal.WriteEntry();
-        }
-    else if (choice == 2) // if they enter 2 they will get displayed an entry
-        {
-            journal.DisplayEntries();
-        }
-    else if (choice == 3)// if they enter 3 they will load past entries
-        {
-            journal.LoadEntries();
-        }
-    else if (choice == 4) // 4 will save entires
-        {
-            journal.SaveEntries();
-        }
-    else if (choice == 5) // this will exit the program and say goodbye
-        {
-            Console.WriteLine("Goodbye!");
-            return;
-        }
-    else // if a number other then 1-5 is picked it will ask the user to try again
-        {
-            Console.WriteLine("Invalid choice, please try again.");
-        }
-    }    
+            if (choice == 1) // if they enter one they write an entry.
+                {
+                    journal.WriteEntry();
+                }
+            else if (choice == 2) // if they enter 2 they will get displayed an entry
+                {
+                    journal.DisplayEntries();
+                }
+            else if (choice == 3)// if they enter 3 they will load past entries
+                {
+                    journal.LoadEntries();
+                }
+            else if (choice == 4) // 4 will save entires
+                {
+                    journal.SaveEntries();
+                }
+            else if (choice == 5) // this will exit the program and say goodbye
+                {
+                    loop = false;
+                    Console.WriteLine("Goodbye!");
+                    return;
+                }
+            else // if a number other then 1-5 is picked it will ask the user to try again
+                {
+                    Console.WriteLine("Invalid choice, please try again.");
+                }
+        }    
     }
     
 
