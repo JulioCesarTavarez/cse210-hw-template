55431c75b2b846f284534ab464c8bc08b8234e64
JulioCesarTavarez <145470440+JulioCesarTavarez@users.noreply.github.com>
2024-06-12 10:13:09 -0600

    breathing class update

--- prove/Develop04/BreathingActiity.cs
+++ prove/Develop04/BreathingActiity.cs
@@ -3,6 +3,15 @@ public class BreathingActivity : Activity
     // Constructor
     public BreathingActivity(string title,string description) : base(title, description)
     {
-        // Activity();
+        Timer time = new();
+        int seconds = time.GetSeconds();
+        int x = 0;
+        float amount = seconds / 6;
+
+        while (x < amount)
+        {
+            Console.WriteLine("Breathe in...");
+            
+        }
     }
 }
